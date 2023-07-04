ef5d1106847058ebe5026024ae78a7048cd0e030
Rizwan Bashir <84863970+Rizwan-Basheer@users.noreply.github.com>
2023-07-04 14:29:10 +0500

    Add toasters and make some other changes

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -7,10 +7,10 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="card"><br><br>
-                        <center>
-                            <img class="rounded-circle avatar-xl" src="{{ asset('backend/assets/images/small/img-5.jpg') }}"
+                        <div class="text-center">
+                            <img class="rounded-circle avatar-xl" src="{{ (!empty(Auth::user()->profile_image))? url('upload/admin_images/'.Auth::user()->profile_image):url('upload/no_image.jpg') }}"
                                 alt="Card image cap">
-                        </center>
+                        </div>
 
                         <div class="card-body">
                             <h4 class="card-title">Name : {{ $adminData->name }} </h4>
@@ -19,7 +19,7 @@
                             <hr>
                             <h4 class="card-title">User Name : {{ $adminData->username }} </h4>
                             <hr>
-                            <a href="{{route('profile.edit')}}" class="btn btn-info btn-rounded waves-effect waves-light"> Edit Profile</a>
+                            <a href="{{ route('edit.profile') }}" class="btn btn-info btn-rounded waves-effect waves-light"> Edit Profile</a>
 
                         </div>
                     </div>
--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -248,7 +248,7 @@
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <img class="rounded-circle header-profile-user"
-                        src="{{ asset('backend/assets/images/users/avatar-1.jpg') }}" alt="Header Avatar">
+                        src="{{ (!empty(Auth::user()->profile_image))? url('upload/admin_images/'.Auth::user()->profile_image):url('upload/no_image.jpg') }}" alt="Header Avatar">
                     <span class="d-none d-xl-inline-block ms-1">{{ Auth::user()->name }}</span>
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                 </button>
